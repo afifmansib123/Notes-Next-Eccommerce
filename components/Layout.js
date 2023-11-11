33cbee548d10f9b2f9f978d54312ca3cdb00093c
afifmansib123 <afifmansib123@gmail.com>
2023-11-11 16:56:54 +0700

    10. Signout - with Dashboard Menu

--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,8 @@ import { Store } from "@/utils/Store"
 import { useContext } from "react"
 import Link from "next/link"
 import { useState, useEffect } from "react"
+import { useSession, signOut } from "next-auth/react"
+import { Menu } from "@headlessui/react"
 
 const Layout = ({title,children}) => {
 
@@ -15,12 +17,20 @@ const Layout = ({title,children}) => {
       setcartitemscount((cart.cartitems.reduce((a, c) => a + c.quantity, 0)))
     },[cart.cartitems])
 
+    const {status, data : session} =  useSession()
+
+    const handlelogout = () => {
+      signOut({ callbackUrl: '/login' })
+      Cookies.remove('cart')
+      dispatch({type : 'CART_RESET'})
+  
+    }
+
     return(
         <div>
             <Head>
             <title>{title? title : "Afif's revision"}</title>
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous"/>
-
             </Head>
             <header className="flex justify-center">
                 CART 
@@ -31,8 +41,46 @@ const Layout = ({title,children}) => {
                     </span>
                   )}
                 </a>
-               <Link legacyBehavior href="/userlogin">LOGIN</Link>
                 <Link href="/cartpage">Check cart</Link>
+                {status === 'loading'
+          ? (<div>Loading</div>)
+          : session?.user
+            ? <Menu>
+              <Menu.Button>{session.user.name}</Menu.Button>
+              <Menu.Items>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      className={`${active && 'bg-blue-500'}`}
+                      href="/profile"
+                    >
+                      Profile
+                    </a>
+                  )}
+                </Menu.Item><br />
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      className={`${active && 'bg-blue-500'}`}
+                      href="/dashboard"
+                    >
+                      Dashbooard
+                    </a>
+                  )}
+                </Menu.Item><br />
+                <Menu.Item>
+                  <a
+
+                    href="#"
+                    onClick={handlelogout} //MAIN SIGN OUT FUNCTIONALITY 
+                  >
+                    Sign out
+                  </a>
+                </Menu.Item>
+              </Menu.Items>
+            </Menu>
+            : (<Link href={'/userlogin'}>LOGIN</Link>)
+        }
             </header>
             <main>
             {children}
