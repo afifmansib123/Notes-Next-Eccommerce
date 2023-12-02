ac03684cb321e16ccede992290b31334a1e440b5
Afif-Mac <afifmansib123@gmail.com>
2023-12-02 17:32:30 +0700

    Admin Dashboard Pt3 - Creating productlist API and show

---
+++ pages/api/admin/allproducts.js
@@ -0,0 +1,23 @@
+import { getSession, useSession } from "next-auth/react";
+import Product from "@/models/productsmodel";
+import db from "@/utils/db";
+
+const handler = async (req,res) => {
+    const session = await getSession({req})
+
+    if(!session || (session && !session.user.isadmin)){
+        return res.status(401).send('admin signin required')
+    }
+
+    await db.connect()
+
+    try{
+        const products = await Product.find({}, '_id name')
+        await db.disconnect()
+        res.status(200).json(products)
+    }catch(err){
+        await db.disconnect()
+        res.status(500).json({message : 'error fetching'})
+    }
+}
+export default handler
